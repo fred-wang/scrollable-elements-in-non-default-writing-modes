79ea4e44918fd869ece36abd152dd2122e8cd330
Frédéric Wang <fwang@igalia.com>
2019-08-07 14:21:25 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", function() {
+    Array.from(document.querySelectorAll(".example")).forEach(div => {
+        var scroller = div.getElementsByClassName("scroller")[0];
+        var input = {
+            scrollLeft: div.getElementsByTagName("input")[0],
+            scrollTop: div.getElementsByTagName("input")[1]
+        };
+        var properties = ["scrollLeft", "scrollTop"];
+        properties.forEach(property => {
+            input[property].value = scroller[property];
+        });
+        properties.forEach(property => {
+            input[property].addEventListener("change", function() {
+                scroller[property] = input[property].value;
+            });
+        });
+        scroller.addEventListener("scroll", function() {
+            ["scrollLeft", "scrollTop"].forEach(property => {
+                properties.forEach(property => {
+                    input[property].value = scroller[property];
+                });
+            });
+        })
+    });
+})
